8cb7acb0de369cdf57291de8e68f5f763771e770
root <6281@holbertonstudents.com>
2023-05-23 05:04:40 -0700

    Fix: the error message -forbids mixed declarations and code [-Werror=declaration-after-statement]- in the file 1-insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,9 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next;
+	listint_t *current;
+
+	current = (*list)->next;
 
 	while (current != NULL)
 	{
